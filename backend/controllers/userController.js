a9722f41b12bf060fc6c2dd58f81756f54c8a686
dpurcell <dpurcell.pd@gmail.com>
2023-08-31 10:22:04 -0400

    user route and controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,27 @@
+//@desc Register new user
+//@route GET /api/users
+//@access Private
 const registerUser = (req, res) => {
     res.json({message: 'Register User'});
+}
+
+//@desc Authenticate a user
+//@route GET /api/users/login
+//@access Public
+const loginUser = (req, res) => {
+    res.json({message: 'Login User'});
+}
+
+
+//@desc Get user data
+//@route GET /api/users/me
+//@access Public
+const getMe = (req, res) => {
+    res.json({message: 'User data display'});
+}
+
+module.exports = {
+   registerUser,
+   loginUser,
+   getMe
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const router = express.router();
-const {registerUser} = require('../controllers/userController');
+const router = express.Router();
+const {registerUser, loginUser, getMe} = require('../controllers/userController');
 
 //create a user
 router.post('/', registerUser);
+router.post('/login', loginUser);
+router.post('/me', getMe);
 
 
 module.exports = router;
