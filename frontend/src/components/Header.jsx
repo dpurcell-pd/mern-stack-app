65893b2ef7547f7dbfae236737a0fc93059a8064
dpurcell <dpurcell.pd@gmail.com>
2023-09-05 23:33:53 -0400

    assignment submission commit

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,25 +1,44 @@
-import {FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa';
-import {Link} from 'react-router-dom';
-
+import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import { UseSelector, useDispatch } from "react-redux";
+import { logout, reset } from "../features/auth/authSlice";
 export default function Header() {
-  return (
-    <header className='header'>
-        <div className='logo'>
-            <Link to='/'>GoalSetter</Link>
-        </div>
-        <ul>
-            <li>
-                <Link to='/login'>
-                    <FaSignInAlt /> Login
-                </Link>
-            </li>
-            <li>
-                <Link to='/register'>
-                    <FaUser /> Register
-                </Link>
-            </li>
-        </ul>
-    </header>
-  )
-}
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
 
+  const onLogout = () => {
+    dispatch(logout())
+    dispatch(reset())
+    navigate('/')
+  }
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">GoalSetter</Link>
+      </div>
+      <ul>
+        {user ? (
+          <li>
+            <button className="btn" onClick={onLogout}>         
+              <FaSignnAlt /> Login
+              </button>              
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">
+                <FaSignInAlt /> Login
+              </Link>
+            </li>
+            <li>
+              <Link to="/register">
+                <FaUser /> Register
+              </Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </header>
+  );
+}
--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -43,9 +43,9 @@ export const register = createAsyncThunk(
 //   }
 // })
 
-// export const logout = createAsyncThunk('auth/logout', async () => {
-//   await authService.logout()
-// })
+export const logout = createAsyncThunk('auth/logout', async () => {
+  await authService.logout()
+})
 
 export const authSlice = createSlice({
   name: 'auth',
@@ -74,23 +74,23 @@ export const authSlice = createSlice({
         state.message = action.payload
         state.user = null
       })
-      // .addCase(login.pending, (state) => {
-      //   state.isLoading = true
-      // })
-      // .addCase(login.fulfilled, (state, action) => {
-      //   state.isLoading = false
-      //   state.isSuccess = true
-      //   state.user = action.payload
-      // })
-      // .addCase(login.rejected, (state, action) => {
-      //   state.isLoading = false
-      //   state.isError = true
-      //   state.message = action.payload
-      //   state.user = null
-      // })
-      // .addCase(logout.fulfilled, (state) => {
-      //   state.user = null
-      // })
+      .addCase(login.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.user = action.payload
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+        state.user = null
+      })
+      .addCase(logout.fulfilled, (state) => {
+        state.user = null
+      })
   },
 })
 
